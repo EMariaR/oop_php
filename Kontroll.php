4c1abaa2fe3e4f5984841dd86c9fd3a471ce0c2b
eMariaR <elo-maria.roots@khk.ee>
2018-12-11 09:59:29 +0200

    Kontroll

--- Kontroll.php
+++ Kontroll.php
@@ -16,4 +16,39 @@ a) defineeritud muutujate abil
 b) Väljastatud muutujate väärtused koos sobiva tekstiga html faili kujul
 */
 
+$minuNimi = 'Elo-Maria Roots';
+$kursuseTähis = 'KD';
+$kursuseNumber = '18';
+$kooliEmail = 'elo-maria.roots@khk.ee';
 
+// muutujate sisu testvaatamine
+var_dump($minuNimi);
+echo '<br>';
+var_dump($kursuseTähis);
+echo '<br>';
+var_dump($kursuseNumber);
+echo '<br>';
+var_dump($kooliEmail);
+
+// andmetüübid
+$taisarv = 7;
+$reaalarv = -2.6;
+$tekst = 'Tere PHP';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+
+// väärtuse tüübi kontroll - gettype($muutujaNimi)
+// väärtuse tüübi teisendus - settype($muutujaNimi, 'tüüp')
+// väljastamine
+// ühendamine 'tekst katkeb '.$muutujaNimi.' tekst jätkub'
+echo '
+    <!doctype html>
+    <html>
+        <head>
+            <title>'.$minuNimi.'</title>
+        </head>
+        <body>
+            <h1>'.$kursuseTähis.'</h1>
+            <h3>'.$kursuseTähis.'</h3>
+            <h3>'.$kooliEmail.'</h3>
+            ';
--- katse.php
+++ katse.php
@@ -9,27 +9,41 @@
 // $muutujaNimi = väärtus;
 $lehePealkiri = 'Katse leht';
 $sisuPealkiri = 'Muutujate defineerimine';
+
 // muutujate sisu testvaatamine
 var_dump($lehePealkiri);
 echo '<br>';
 var_dump($sisuPealkiri);
+
 // andmetüübid
 $taisarv = 7;
 $reaalarv = -2.6;
 $tekst = 'Tere PHP';
 $toevaartusTrue = true;
 $toevaartusFalse = false;
+
 // väärtuse tüübi kontroll - gettype($muutujaNimi)
 // väärtuse tüübi teisendus - settype($muutujaNimi, 'tüüp')
 // väljastamine
 // ühendamine 'tekst katkeb '.$muutujaNimi.' tekst jätkub'
-echo '
+
+// tingimuslaused
+/*
+ * if (tingimus == true) {
+ *    siis toimub see osa
+ * } else {
+ *  muidu toimib see osa
+ * }
+ **/
+$näitaSisu = true;
+echo ' 
     <!doctype html>
     <html>
         <head>
             <title>'.$lehePealkiri.'</title>
         </head>
         <body>
+        if($naitaSisu == true) {      
             <h1>'.$sisuPealkiri.'</h1>
             <h3>Andmetüübid</h3>
             <ul>
@@ -47,6 +61,7 @@ settype($taisarv, 'string');
 echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
 echo '<br>';
 echo gettype($taisarv);
+}
 // html lehe lõpp
 echo '
         </body>
