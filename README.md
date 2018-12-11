2337a4b6302a35561ec0b6fc80bb12b5f52df19f
eMariaR <elo-maria.roots@khk.ee>
2018-12-11 09:13:21 +0200

    Readme parandus

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 ## Tarkvara
 * [git] (https://git-scm.com/download/win) versioonihaldussüsteemina (VCS)
 * [php] (http://php.net/)- PHP interpretaator
-*[PhpStorm] (https://www.jetbrains.com/phpstorm/)- IDEA
+* [PhpStorm] (https://www.jetbrains.com/phpstorm/)- IDEA
--- katse.php
+++ katse.php
@@ -1,19 +1,44 @@
 <?php
 /**
- * Created by PhpStorm.
+// * Created by PhpStorm.
  * User: elo-maria.roots
  * Date: 06.12.2018
  * Time: 11:08
  */
-
+// muutujate defineerimine
+// $muutujaNimi = väärtus;
+$lehePealkiri = 'Katseleht*;
+$sisuPealkiri = 'Muutujate defineerimine';
+// muutujate sisu testvaatamine
+var_($lehePealkiri);
+echo '<br>;
+var_dump($sisuPealkiri);
+//andmetüübid
+$täisarv = 7;
+$reaalarv = -2.6;
+$tekst = 'Tere PHP';
+$tõeväärtusTrue = true;
+$tõeväärtusFalse = false;
+//väärtuse tüübi kontroll - gettype($muutujaNimi)
+//väärtuse tüübi teisendus - settype($muutujaNimi, 'tüüp')
+//väljastamine
+//ühendamine 'tekst katkeb'.$muutujaNimi.' tekst jätkub'
 echo  '
    <!doctype html>
    <html>
       <head>
-          <title></title>
+          <title>'.lehePealkiri.''</title>
       </head>
         <body>
-           <h1> PHP abil loodud HTML</h1>
+           <h1>'.sisuPealkiri.'</h1>
+<h3>Andmetüübid</h3>
+<ul>
+   <li>$täisarv = '.$täisarv.'</li>
+   <li>$reaalarv = '.$reaalarv.'</li>
+   <li>$tekst = '.@tekst.'</li>
+   <li>$tõeväärtusTrue = '.$tõeväärtusTrue.'</li>
+   <li>$tõeväärtusFalse = '.$tõeväärtusFalse.'</li>
+</ul>
         </body>
     </html>
 ';
